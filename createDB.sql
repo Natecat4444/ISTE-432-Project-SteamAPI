8727fd529ace6a16b4c0aaaec66c0ef4001a977f
MikeReniere <mr3860@g.rit.edu>
2020-02-28 14:33:04 -0500

    made changes

--- createDB.sql
+++ createDB.sql
@@ -4,12 +4,12 @@ create database login;
 
 use login;
 
-create table login(username varchar(255), pass varchar(255), id int NOT NULL, CONSTRAINT login_pk PRIMARY KEY (id));
+create table userInfo(username varchar(255), pass varchar(255), id int NOT NULL, CONSTRAINT login_pk PRIMARY KEY (id));
 -- ID will be a 6 character length of numbers
 create table favorites(gameName varchar(255), id int NOT NULL, CONSTRAINT fav_pk PRIMARY KEY (id));
 
-INSERT INTO login VALUES ("testUser", "password", 123456);
+INSERT INTO userInfo VALUES ("testUser", "password", 123456);
 INSERT INTO favorites VALUES ("Counter-Strike: Global Offensive", 123456);
 
-select * from login;
+select * from userInfo;
 select * from favorites;
