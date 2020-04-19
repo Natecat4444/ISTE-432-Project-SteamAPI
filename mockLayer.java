29fb496a730fdcfed6988f70eb3476cd6505c064
MikeReniere <mr3860@g.rit.edu>
2020-04-19 14:30:12 -0400

    Add files via upload

--- mockLayer.java
+++ mockLayer.java
@@ -5,20 +5,25 @@ import org.junit.runners.*;
 
 
 public class mockLayer { 
-     private Statement mockStatement;
-      private Connection mockConn;
+   private Statement mockStatement;
+   private Connection mockConn;
    
-  public static void main(String args[]){
+   public static void main(String args[]){
       dbLayer db;
       db = mock(dbLayer.class);
-      System.out.println(db);
+   //   System.out.println(db);
       when(db.connect()).thenReturn(true);
       if(db.connect() == true ){
          System.out.println("TRUE TEST");
          System.out.println("Testing LOGIN with correct password");
          
        //  when(db.login("testuser", "password")).thenReturn(false);
-         db.login("testuser", "password");
+      boolean test = db.login("testuser", "password");
+      System.out.println("test output: " + test); // This returns false because we are using the mocked dbLayer.class
+      //  mock(db.login());
+      //when(db.login()).thenReturn(true);
+         ArrayList<ArrayList<String>> testArray = db.getData("Aaa", "AAA");
+         System.out.println(testArray);
       }else{
          System.out.println("TEST FALSE");
       
