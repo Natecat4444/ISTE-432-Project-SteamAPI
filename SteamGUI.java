18d8b8438ae00d4b57ebfb2c38dcdc88b3ef54e2
Nathan Kaufman <nlk7665@rit.edu>
2020-04-23 19:36:00 -0400

    This is only some of the changes to the GUI

--- SteamGUI.java
+++ SteamGUI.java
@@ -12,6 +12,8 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
 
 
 public class SteamGUI extends Application{
@@ -19,9 +21,12 @@ public class SteamGUI extends Application{
    private static String Username = "";
    private static String Password = "";
    private static BorderPane prime;
-   // private static BusinessLayer BL;
+   private static BusinessLayer BL;
    private static TextField userIn;
    private static PasswordField passIn;
+   private static TabPane tabs;
+   private static dbLayer db;
+   private static Boolean loggedIn;
    
    private void setUsername(String Username){
       this.Username = Username;
@@ -36,7 +41,49 @@ public class SteamGUI extends Application{
       return false;
    }
    
+   private void regesterWindow(){
+      Label resLabel = new Label("Choose a Username/Password");
+      prime.setTop(resLabel);
+      userIn = new TextField();
+      passIn = new PasswordField();
+      
+      Label label1 = new Label("Username: ");
+      Label label2 = new Label("Password: ");
+      
+      HBox HBox1 = new HBox(label1, userIn);
+      
+      HBox HBox2 = new HBox(label2, passIn);
+      
+      Button loginbtn = new Button();
+      loginbtn.setText("Regester");
+      loginbtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                setUsername(userIn.getText());
+                setPassword(passIn.getText());
+                prime.setCenter(loginWindow());
+            }
+        });
+        
+      Button guestbtn = new Button();
+      guestbtn.setText("Continue as Guest");
+      guestbtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+         @Override
+         public void handle(MouseEvent event){
+            setUpMainGuest();
+         }
+      
+      });
+      
+      HBox buttons = new HBox(loginbtn, guestbtn);
+      
+      VBox regester = new VBox(HBox1, HBox2, buttons);
+      prime.setCenter(regester);
+   }
+   
    private VBox loginWindow(){
+      Label logLabel = new Label("Enter a Username/Password");
+      prime.setTop(logLabel);
       userIn = new TextField();
       passIn = new PasswordField();
       
@@ -55,6 +102,7 @@ public class SteamGUI extends Application{
                 setUsername(userIn.getText());
                 setPassword(passIn.getText());
                 if(loginSuccess(Username, Password)){
+                  loggedIn = true;
                   setUpMain();
                 }
                 else{
@@ -74,18 +122,30 @@ public class SteamGUI extends Application{
       
       });
       
-      HBox buttons = new HBox(loginbtn, guestbtn);
+      Button RegesterBtn = new Button();
+      RegesterBtn.setText("Create Account");
+      RegesterBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+         @Override
+         public void handle(MouseEvent event){
+            regesterWindow();
+         }
+      
+      });
+      
+      HBox buttons = new HBox(loginbtn, guestbtn, RegesterBtn);
       
       VBox login = new VBox(HBox1, HBox2, buttons);
       return login;
    }
    
    public void start(Stage primaryStage){
-      // BL = new BuisnessLayer();
+      BL = new BusinessLayer();
+      loggedIn = false;
       this.primaryStage = primaryStage;
       prime = new BorderPane();
       prime.setCenter(loginWindow());
       Scene scene = new Scene(prime);
+      primaryStage.setScene(scene);
       primaryStage.show();
    }
    
