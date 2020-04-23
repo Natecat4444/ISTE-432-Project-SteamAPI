6de7be286ecab93cd6e9b7e4d136b442ee175f0f
MikeReniere <mr3860@g.rit.edu>
2020-04-23 13:33:08 -0400

    changed sql file to include favorites table

--- createDB.sql
+++ createDB.sql
@@ -4,11 +4,12 @@ create database login;
 
 use login;
 
-create table userInfo(username varchar(255), pass varchar(255), id int NOT NULL, CONSTRAINT login_pk PRIMARY KEY (id));
+create table userInfo(username varchar(255), pass varchar(255), CONSTRAINT login_pk PRIMARY KEY (username));
 -- ID will be a 6 character length of numbers
-create table favorites(gameName varchar(255), id int NOT NULL, CONSTRAINT fav_pk PRIMARY KEY (id));
+create table favorites(username varchar(255), appid int NOT NULL, CONSTRAINT fav_pk PRIMARY KEY (username), FOREIGN KEY (username) REFERENCES userInfo(username));
 
-INSERT INTO userInfo VALUES ("testUser", "sha1:64000:18:IdVIuuzbN0KOXgAdAnGuN8HLj2c1FjyL:SmuyqX58OLjTjM9N5GtrlnLU", 123456);
-INSERT INTO favorites VALUES ("Counter-Strike: Global Offensive", 123456);
+INSERT INTO userInfo VALUES ("testUser", "sha1:64000:18:GPDurdNt3uRWMf8JA50WME8p+D/eTv2a:+Pxh8FZ7OrzG5EsTDinUivkb");
+INSERT INTO favorites VALUES ("testUser", 10);
 
 select * from userInfo;
+select * from favorites;
